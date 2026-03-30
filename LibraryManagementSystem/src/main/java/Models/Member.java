5fbc7bf88e183915a6f80f3926dfe33f0d76ea01
Paul Antofe <paul.antofe@gmail.com>
2026-03-30 15:05:42 +0300

    Developed the member class with getters and printers

--- LibraryManagementSystem/src/main/java/Models/Member.java
+++ LibraryManagementSystem/src/main/java/Models/Member.java
@@ -1,5 +1,27 @@
 package Models;
 
 public class Member {
-    
+    String firstName;
+    String secondName;
+    String CNP;
+
+    public void printMemberName() {
+        System.out.println(firstName + secondName);
+    }
+
+    public void printCNP() {
+        System.out.println(CNP);
+    }
+
+    public void getFirstName(String name) {
+        firstName = name;
+    }
+
+    public void getSecondName(String name) {
+        secondName = name;
+    }
+
+    public void getCNP(String cnp) {
+        CNP = cnp;
+    }
 }
